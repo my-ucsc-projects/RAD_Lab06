3663d72a58cb3462ab0782f03f2106bbe8e43c13
Ravindu Hasanka <rhatu2000@gmail.com>
2023-07-23 23:10:38 +0530

    lab session practice

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <h1></h1> */}
+      <h1>Welcome to My React App</h1>
+      <p>This is my first React application</p>
     </div>
   );
 }
